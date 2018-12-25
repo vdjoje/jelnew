7b83ef827be3815208d34bbe6f5ee2db7dcb4125
jevdjauon <aleksandar@uon.rs>
2018-12-25 11:12:12 +0100

    work

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { Menu, Image } from "semantic-ui-react";
+import { Image, Button, Icon } from "semantic-ui-react";
 import logo from "../../logo.svg";
-// import { Link } from "react-scroll";
 import { Link } from "react-scroll";
 import InlineSVG from "svg-inline-react";
 import { arrowDown } from "../../assets/svg";
@@ -44,10 +43,8 @@ export default class Topbar extends Component {
             </a>
           </nav>
           <Image
-            // ui="true"
             src={logo}
             id="header--logo"
-            // className="App-logo"
             alt="logo"
             size="small"
             as={Link}
@@ -66,7 +63,61 @@ export default class Topbar extends Component {
           </nav>
         </div>
         {this.state.submenuVisible ? (
-          <div className="header--submenu">aaasd</div>
+          <div className="header--submenu">
+            <div className="header--subnav">
+              <h3>01.</h3>
+              <a href="/oaza">Poslovni Apartmanski Objekat ‘OAZA’</a>
+              <h3>02.</h3>
+              <a href="/ambasada">
+                Ambasada UAE u Capital Plaza Centru (Podgorica)
+              </a>
+              <h3>03.</h3>
+              <a href="/atlas">
+                Atlas banka u Capital Plaza Centru (Podgorica)
+              </a>
+              <h3>04.</h3>
+              <a href="/bogdanov">
+                Stambeno-poslovni objekat “Bogdanov kraj” (Cetinje)
+              </a>
+              <h3>05.</h3>
+              <a href="/benexfitnes">
+                “Benex fitnes centar” u Capital Plaza Centru (Podgorica)
+              </a>
+              <h3>06.</h3>
+              <a href="/stambenicetinje">Stambeni objekat (Cetinje)</a>
+              <h3>07.</h3>
+              <a href="/vila">Vila na primorju</a>
+              <h3>08.</h3>
+              <a href="/planinski">Planinski stan</a>
+              <div id="header--kontakt">
+                <a href="tel:382-67-254-844">+382 67 / 254 - 844</a>
+                <br />
+                <a
+                  // className="kontakt--email"
+                  title="My email"
+                  href="mailto:arhmiskovic@gmail.com"
+                >
+                  arhmiskovic@gmail.com
+                </a>
+              </div>
+              <div className="header--icons">
+                <Button
+                  id="header--button"
+                  href="https://www.facebook.com/jelena.miskovic.50"
+                  target="_blank"
+                >
+                  <Icon size="large" name="facebook f" />
+                </Button>
+                <Button
+                  id="header--button"
+                  href="https://www.instagram.com/ena_jel/"
+                  target="_blank"
+                >
+                  <Icon size="large" name="instagram" />
+                </Button>
+              </div>
+            </div>
+          </div>
         ) : null}
       </header>
 
@@ -139,40 +190,8 @@ export default class Topbar extends Component {
       //       >
       //         &times;
       //       </a>
-      //       <div className="sideOptions">
-      //         <a className="menu-item" href="/oaza">
-      //           Oaza
-      //         </a>
 
-      //         <a className="menu-item" href="/ambasada">
-      //           Ambasada UAE
-      //         </a>
-
-      //         <a className="menu-item" href="/atlas">
-      //           Atlas banka
-      //         </a>
-
-      //         <a className="menu-item" href="/bogdanov">
-      //           Bogdanov kraj
-      //         </a>
-
-      //         <a className="menu-item" href="/benexfitnes">
-      //           Benex fitnes centar
-      //         </a>
-
-      //         <a className="menu-item" href="/stambenicetinje">
-      //           Stambeni objekat
-      //         </a>
-
-      //         <a className="menu-item" href="/vila">
-      //           Vila na primorju
-      //         </a>
-
-      //         <a className="menu-item" href="/planinski">
-      //           Planinski stan
-      //         </a>
-      //       </div>
-      //     </div>
+      // </div>
       //   </Menu>
       // </div>
     );

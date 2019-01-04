1c70d747c009ca3e4bc9d7f6d1c50453d3d19f1f
vdjoje <jevdjakanarevo@yahoo.com>
2019-01-04 16:25:36 +0100

    test

--- src/assets/pictures.js
+++ src/assets/pictures.js
@@ -1,47 +1,41 @@
-import o2 from "../images/Oaza/2A.jpg"; //
-import o3 from "../images/Oaza/3A.jpg"; //
-import o4 from "../images/Oaza/4A.jpg"; //
-import o6 from "../images/Oaza/6.jpg"; //
-import o7 from "../images/Oaza/7A.jpg"; //
-import o8 from "../images/Oaza/8A.jpg"; //
-import am1 from "../images/Ambasada/ambasada-UAE.jpg"; //
-import am2 from "../images/Ambasada/ambasada-UAE2.jpg"; //
-import am5 from "../images/Ambasada/ambasada-UAE5.jpg"; //
-import am6 from "../images/Ambasada/ambasada-UAE6.jpg"; //
-import a4 from "../images/Atlas/s4.jpg"; //
-import a5 from "../images/Atlas/s5.jpg"; //
-import a7 from "../images/Atlas/s7.jpg"; //
-import a9 from "../images/Atlas/s9.jpg"; //
-import b2 from "../images/Bogdanov/2.jpg"; //
-import b3 from "../images/Bogdanov/3.jpg"; //
-import b5 from "../images/Bogdanov/5.jpg"; //
-import b8 from "../images/Bogdanov/8.jpg"; //
-import bf2 from "../images/Benex/04.jpg"; //
-import bf3 from "../images/Benex/01.jpg"; //
-import bf4 from "../images/Benex/02.jpg"; //
-import bf5 from "../images/Benex/03.jpg"; //
-import s1 from "../images/StambeniCetinje/1.jpg"; //
-import s2 from "../images/StambeniCetinje/2.jpg"; //
-import s3 from "../images/StambeniCetinje/3.jpg"; //
-import s4 from "../images/StambeniCetinje/4.jpg"; //
-import v1 from "../images/Vila/1.jpg"; //
-import v3 from "../images/Vila/3.jpg"; //
-import v4 from "../images/Vila/4.jpg"; //
-import v5 from "../images/Vila/5.jpg"; //
-import v7 from "../images/Vila/7.jpg"; //
-import v8 from "../images/Vila/8.jpg"; //
-import p1 from "../images/Planinski/1.jpg"; //
-import p2 from "../images/Planinski/2.jpg"; //
-import p3 from "../images/Planinski/3.jpg"; //
-import p4 from "../images/Planinski/4.jpg"; //
+import o2 from "../images/Oaza/2A.jpg";
+import o3 from "../images/Oaza/3A.jpg";
+import o4 from "../images/Oaza/4A.jpg";
+import o8 from "../images/Oaza/8A.jpg";
+import am1 from "../images/Ambasada/ambasada-UAE.jpg";
+import am2 from "../images/Ambasada/ambasada-UAE2.jpg";
+import am5 from "../images/Ambasada/ambasada-UAE5.jpg";
+import am6 from "../images/Ambasada/ambasada-UAE6.jpg";
+import a4 from "../images/Atlas/s4.jpg";
+import a5 from "../images/Atlas/s5.jpg";
+import a7 from "../images/Atlas/s7.jpg";
+import a9 from "../images/Atlas/s9.jpg";
+import b2 from "../images/Bogdanov/2.jpg";
+import b3 from "../images/Bogdanov/3.jpg";
+import b5 from "../images/Bogdanov/5.jpg";
+import b8 from "../images/Bogdanov/8.jpg";
+import bf2 from "../images/Benex/04.jpg";
+import bf3 from "../images/Benex/01.jpg";
+import bf4 from "../images/Benex/02.jpg";
+import bf5 from "../images/Benex/03.jpg";
+import s1 from "../images/StambeniCetinje/1.jpg";
+import s2 from "../images/StambeniCetinje/2.jpg";
+import s3 from "../images/StambeniCetinje/3.jpg";
+import s4 from "../images/StambeniCetinje/4.jpg";
+import v1 from "../images/Vila/1.jpg";
+import v3 from "../images/Vila/3.jpg";
+import v4 from "../images/Vila/4.jpg";
+import v5 from "../images/Vila/5.jpg";
+import p1 from "../images/Planinski/1.jpg";
+import p2 from "../images/Planinski/2.jpg";
+import p3 from "../images/Planinski/3.jpg";
+import p4 from "../images/Planinski/4.jpg";
 
 export default {
   oaza: [
     { src: o2, width: 6, height: 4 },
     { src: o3, width: 6, height: 4 },
     { src: o4, width: 6, height: 4 },
-    { src: o6, width: 6, height: 4 },
-    { src: o7, width: 6, height: 4 },
     { src: o8, width: 6, height: 4 }
   ],
 
@@ -70,7 +64,7 @@ export default {
     { src: bf2, width: 6, height: 4 },
     { src: bf3, width: 6, height: 4 },
     { src: bf4, width: 6, height: 4 },
-    { src: bf5, width: 6, height: 4 }
+    { src: bf5, width: 6, height: 6 }
   ],
 
   stambeni: [
@@ -84,9 +78,9 @@ export default {
     { src: v1, width: 6, height: 4 },
     { src: v3, width: 6, height: 4 },
     { src: v4, width: 6, height: 4 },
-    { src: v5, width: 6, height: 4 },
-    { src: v7, width: 6, height: 4 },
-    { src: v8, width: 6, height: 4 }
+    { src: v5, width: 6, height: 4 }
+    // { src: v7, width: 6, height: 4 },
+    // { src: v8, width: 6, height: 4 }
   ],
 
   planinski: [
--- src/switches/Main/Main.jsx
+++ src/switches/Main/Main.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../../pages/Home";
-// import Oaza from "../../components/Portfolio/Oaza";
-// import Ambasada from "../../components/Portfolio/Ambasada";
-// import Atlas from "../../components/Portfolio/Atlas";
-// import Benexfitnes from "../../components/Portfolio/Benexfitnes";
-// import Bogdanov from "../../components/Portfolio/Bogdanov";
-// import Vila from "../../components/Portfolio/Vila";
-// import Planinski from "../../components/Portfolio/Planinski";
-// import StambeniCetinje from "../../components/Portfolio/StambeniCetinje";
 import Portfolio from "../../components/Portfolio/Portfolio";
 import pictures from "../../assets/pictures";
 
